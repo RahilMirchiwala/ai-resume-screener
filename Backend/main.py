2eb6931aaed94b32e9400ed035962462208ca9c1
Rahil <rahilmirchiwala21@gmail.com>
2026-04-29 10:27:01 +0530

    fix: cors and dictionary keys

--- Backend/main.py
+++ Backend/main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI,UploadFile,File,Form
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
 from parser import extract_text_from_pdf
 from extractor import extract_skills
 from matcher import calculate_match
 from explainer import generate_explanation
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -21,30 +21,29 @@ def root():
 
 @app.post("/analyze")
 async def analyze(
-  resume: UploadFile = File(...),
-  job_description: str = Form(...)
+    resume: UploadFile = File(...),
+    job_description: str = Form(...)
 ):
-  
-  pdf_bytes = await resume.read()
+    pdf_bytes = await resume.read()
 
-  resume_text = extract_text_from_pdf(pdf_bytes)
+    resume_text = extract_text_from_pdf(pdf_bytes)
 
-  resume_skills = extract_skills(resume_text,source="resume")
-  job_skills = extract_skills(job_description,source="job_description")
+    resume_skills = extract_skills(resume_text, source="resume")
+    job_skills = extract_skills(job_description, source="job_description")
 
-  result = calculate_match(resume_skills,job_skills)
+    result = calculate_match(resume_skills, job_skills)
 
-  explanation = generate_explanation(
-    resume_skills=resume_skills,
-    job_skills=job_skills,
-    matched_skills=result["Matched Skills"],
-    missing_skills=result["Missing Skills"],
-    score=result["Score"]
-  )
+    explanation = generate_explanation(
+        resume_skills=resume_skills,
+        job_skills=job_skills,
+        matched_skills=result["matched_skills"],
+        missing_skills=result["missing_skills"],
+        score=result["score"]
+    )
 
-  return {
-    "score": result['Score'],
-    "matched_skills": result['Matched Skills'],
-    "missing_skills": result['Missing Skills'],
-    "explanation": explanation
-  }
+    return {
+        "score": result["score"],
+        "matched_skills": result["matched_skills"],
+        "missing_skills": result["missing_skills"],
+        "explanation": explanation
+    }
